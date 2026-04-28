217cb9f428229967f09d828bf1c16c99078849b8
dewansh-bit <dewanshatwork@gmail.com>
2026-04-28 17:09:54 +0000

    Updated inventory service and saved latest changes

--- inventory-service/app.py
+++ inventory-service/app.py
@@ -26,4 +26,4 @@ def get_inventory_by_product(product_id):
     return jsonify({"error": "Inventory not found"}), 404
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
